12d7fb073651f1b9860425078703d3dc01feab6e
j.webapp <j.webprogram@gmail.com>
2020-08-19 07:56:59 +0000

    view

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,6 +2,6 @@ class TopsController < ApplicationController
   def index
     @posts_view = Post.order(impressions_count: 'DESC').first(4)
     @posts_latest = Post.order(created_at: 'DESC').first(4)
-    @tags = Post.tag_counts.first(3)
+    @tags = Post.tag_counts
   end
 end
